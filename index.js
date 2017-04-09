2308bb8c54d1f636355da1248f6cdeb55955bc2a
yannunes <yannunes@gmail.com>
2017-04-08 21:57:39 -0300

    Scraper testing for building the unified API is OK

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const mainExport = {
-  logBuild: () => console.log(console.log('Build was successful1'))
+const googleScraper = require('google-play-scraper')
+const appleScraper = require('app-store-scraper')
+const q = require('q')
+
+var getPvZGoogle = () => {
+  var dfd = q.defer()
+  googleScraper
+    .app({appId: 'com.dxco.pandavszombies'})
+    .then((result) => {
+      dfd.resolve(result)
+    })
+    .catch((err)=>{
+      dfd.reject(err)
+    })
+  return dfd.promise
 }
 
-module.exports = mainExport
+var getPvZApple = () => {
+  var dfd = q.defer()
+  appleScraper
+    .app({id: 403858572})
+    .then((result) => {
+      dfd.resolve(result)
+    })
+    .catch((err)=>{
+      dfd.reject(err)
+    })
+  return dfd.promise
+}
+
+module.exports = {
+  getPvZGoogle: getPvZGoogle,
+  getPvZApple: getPvZApple
+}
--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,9 +1,53 @@
 const chai = require('chai')
 const mainFile = require('../index.js')
 
-describe('Successful first build', () => {
-  it('Should log that index.js was built successfuly', (done) => {
-    mainFile.logBuild()
-    done()
+var expect = chai.expect
+
+/**
+ * Old tests won't be deleted, only commented. Please, let these old tests be,
+ * they will only throw errors if you uncomment them
+ */
+
+// First Build Test
+// describe('Successful first build', () => {
+//   it('Should equal to 3', (done) => {
+//     expect(mainFile.logBuild()).to.equal(3)
+//     done()
+//   })
+// })
+
+describe('Testing the Google Scraper API', () => {
+  describe('Testing the App Function to retrive the app Info', () => {
+    it('Should return the App Info for Plants Vs Zombies', (done) => {
+      mainFile.getPvZGoogle()
+      .then((result) => {
+        console.log(result)
+        expect(result).to.be.an('object')
+        done()
+      })
+      .catch((err) => {
+        console.log(err)
+        expect(err).to.equal(null)
+        done()
+      })
+    })
+  })
+})
+
+describe('Testing the Apple Scraper API', () => {
+  describe('Testing the App Function to retrive the app Info', () => {
+    it('Should return the App Info for Plants Vs Zombies', (done) => {
+      mainFile.getPvZApple()
+      .then((result) => {
+        console.log(result)
+        expect(result).to.be.an('object')
+        done()
+      })
+      .catch((err) => {
+        console.log(err)
+        expect(err).to.equal(null)
+        done()
+      })
+    })
   })
 })
