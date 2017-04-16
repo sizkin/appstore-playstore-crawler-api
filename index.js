6ebaadb8cbaa5a36b03a8c97f19769bcc51139d6
yannunes <yannunes@gmail.com>
2017-04-16 12:46:14 -0300

    Moved google functions to another file

--- index.js
+++ index.js
@@ -1,172 +1,10 @@
-const googleScraper = require('google-play-scraper')
 const appleScraper = require('app-store-scraper')
-const async = require('async')
 const _ = require('lodash')
 const q = require('q')
+const async = require('async')
 
-/**
- * Get search result by entering name of App
- * from Google Play Store.
- * @function
- * @param {string} appName - Name of App
- * @returns promise
- */
-var getGoogleSearchResult = (appName) => {
-  var dfd = q.defer()
-  appName = appName || 'Plants Vs Zombies'
-  googleScraper
-    .search({
-      term: appName,
-      num: 250
-    })
-    .then(result => {
-      dfd.resolve(result)
-    })
-    .catch(error => {
-      dfd.reject(error)
-    })
-
-  return dfd.promise
-}
-
-/**
- * Gets list of top 200 apps from category/collection
- * its ID
- * @function
- * @param {object} options - App Options
- * @returns promise
- */
-var getEntireListOfCategoryGoogle = (listOptions) => {
-  var dfd = q.defer()
-
-  listOptions.start = 0
-  listOptions.num = 120
-  listOptions.throttle = 1
-
-  var index = listOptions.start
-
-  var funcArray = [
-    (callback) => {
-      var list = []
-      googleScraper
-        .list(listOptions)
-        .then(result => {
-          // console.log('ok ' + index)
-          list = _.concat(list, result)
-          listOptions.start = list.length
-          index = list.length
-          listOptions.num = 80
-          callback(null, list, listOptions)
-        })
-        .catch(err => {
-          console.log('error at index ' + index)
-          console.log(err)
-          callback(err)
-        })
-    },
-    (list, listOptions, callback) => {
-      googleScraper
-        .list(listOptions)
-        .then(result => {
-          // console.log('ok ' + index)
-          list = _.concat(list, result)
-          listOptions.start = list.length
-          index = list.length
-          callback(null, list, listOptions)
-        })
-        .catch((err) => {
-          // console.log('error at index ' + index)
-          console.log(JSON.stringify(err))
-          callback(err)
-        })
-    }
-  ]
-  async.waterfall(
-    funcArray,
-    (err, result) => err ? dfd.reject(err) : dfd.resolve(result)
-  )
-  return dfd.promise
-}
-
-/**
- * Gets ranking of app from Googe Play Store by
- * its ID
- * @function
- * @param {object} options - App Options
- * @returns promise
- */
-var getGoogleRanking = (options) => {
-  var dfd = q.defer()
-
-  options = options || {}
-
-  options = {
-    appId: options.appId || 'com.halfbrick.fruitninja',
-    lang: options.lang || 'en',
-    country: options.country || 'us'
-  }
-
-  var funcArray = [
-    (callback) => {
-      googleScraper
-        .app(options)
-        .then(result => {
-          // console.log(result)
-          var listOptions = {
-            category: googleScraper.category[result],
-            lang: options.lang,
-            country: options.country
-          }
-
-          listOptions.category = googleScraper.category[result.genreId]
-          listOptions.collection = result.free ?
-            googleScraper.collection.TOP_FREE :
-            googleScraper.collection.TOP_PAID
-          // console.log(listOptions.category)
-          // console.log(listOptions)
-          // console.log(result)
-          callback(null, listOptions, result)
-        })
-        .catch(err => callback(err))
-    },
-    (listOptions, appInfo, callback) => {
-      getEntireListOfCategoryGoogle(listOptions)
-        .then(result => {
-          appInfo.rank = _.findIndex(result, (app) => app.appId === appInfo.appId) + 1
-          callback(null, listOptions, appInfo)
-        })
-        .catch(err => callback(err))
-    },
-    (listOptions, appInfo, callback) => {
-      if (appInfo.genreId.indexOf('GAME') > -1) {
-        listOptions.category = googleScraper.category.GAME
-        getEntireListOfCategoryGoogle(listOptions)
-          .then(result => {
-            appInfo.rankOverall = _.findIndex(result, (app) => app.appId === appInfo.appId) + 1
-            callback(null, appInfo)
-          })
-          .catch(err => callback(err))
-      } else if (appInfo.genreId.indexOf('FAMILY') > -1) {
-        listOptions.category = googleScraper.category.FAMILY
-        getEntireListOfCategoryGoogle(listOptions)
-          .then(result => {
-            appInfo.rankOverall = _.findIndex(result, (app) => app.appId === appInfo.appId) + 1
-            callback(null, appInfo)
-          })
-          .catch(err => callback(err))
-      } else {
-        callback(null, appInfo)
-      }
-    }
-  ]
-
-  async.waterfall(
-    funcArray,
-    (err, result) => err ? dfd.reject(err) : dfd.resolve(result)
-  )
-
-  return dfd.promise
-}
+const googleLib = require('./src/google.js')
+// const appleLib = require('./src/appe.js')
 
 /**
 * Get search result by entering name of App
@@ -355,11 +193,7 @@ var getFuncArrayAppleRanking = (appDetails, options) => {
 }
 
 module.exports = {
-  google: {
-    getSearchResult: getGoogleSearchResult,
-    getEntireListOfCategory: getEntireListOfCategoryGoogle,
-    getRanking: getGoogleRanking
-  },
+  google: googleLib,
   apple: {
     getSearchResult: getAppleSearchResult,
     getEntireListOfCategory: getEntireListOfCategoryApple,
